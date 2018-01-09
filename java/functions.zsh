b0b61fd5c35c1dcf54d55afd10573b7c8ec91225
Colin Davis <codavis@changehealthcare.com>
2018-01-09 10:29:24 -0800

    removing a lot of cruft

--- java/functions.zsh
+++
@@ -1,8 +0,0 @@
-gradle() {
-    if [ -x ./gradlew ]
-    then
-        ./gradlew "$@"
-    else
-        ~/.dotfiles/bin/gradle "$@"
-    fi
-}
--- system/env.zsh
+++
@@ -1,3 +0,0 @@
-export BUILD_NUMBER=cwd
-export EDITOR=vim
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
